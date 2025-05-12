0c51312ef970657926fe671d04ce896ab08ea6f6
Patrick M <patrickmatias.silva@hotmail.com>
2025-05-11 23:21:54 -0300

    feat: add dead filter buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,44 @@
 "use client";
 
+import { useRef } from "react";
 import { useReport } from "./hooks/useReport";
 import { columns } from "./reports/columns";
 import { DataTable } from "@/components/ui/data-table";
+import { LoadingScreen } from "@/pages/loading-screen";
+import { Button } from "@/components/ui/button";
+import { ClockPlus } from "lucide-react";
+import { DatePicker, DateRangePicker } from "@/components/date-picker";
 
 export default function Home() {
-  const { report, loading, error } = useReport();
+    const { report, loading, error } = useReport();
+    const dateRangePickerTriggerRef = useRef<HTMLButtonElement>(null);
 
-  if (loading) return <p>Loading report…</p>;
-  if (error) return <p>Error loading report: {error}</p>;
-  if (!report) return <p>No report available.</p>;
+    if (loading) return <LoadingScreen />;
+    if (error) return <p>Error loading report: {error}</p>;
+    if (!report) return <p>No report available.</p>;
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-inter)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
-          Payments
-        </h1>
-        <DataTable columns={columns} data={report} />
-      </main>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-16 font-[family-name:var(--font-inter)]">
+            <header className="flex justify-between items-center gap-4">
+                <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
+                    Payments
+                </h1>
+                <div className="flex items-center gap-4">
+                    <DatePicker />
+                    {/* Render the DateRangePicker with a hidden trigger */}
+                    <DateRangePicker triggerRef={dateRangePickerTriggerRef} />
+                    <Button 
+                        onClick={() => dateRangePickerTriggerRef.current?.click()} 
+                        variant="outline"
+                    >
+                        <ClockPlus />
+                        Add Period
+                    </Button>
+                </div>
+            </header>
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <DataTable columns={columns} data={report} />
+            </main>
+        </div>
+    );
 }
