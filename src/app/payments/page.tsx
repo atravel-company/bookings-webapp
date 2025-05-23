dea3d610ad6ff90d279518066cd1f5a731833ff2
Patrick <99044724+patrickkmatias@users.noreply.github.com>
2025-05-23 14:08:04 -0300

    feat: add mock data fetching; serverside data fetching and rendering  (#2)
    * refactor: move routes service to `/lib`
    
    * refactor: better routes provider
    
    * feat: add mock data fetching; serverside data fetching and rendering
    
    before only data fetching was being done serverside
    
    * chore: delete unused file

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -2,50 +2,38 @@ import { columns } from "../../components/bookings-table/columns";
 import { BookingsTable } from "@/components/bookings-table";
 import DateFilters from "./partials/date-filters";
 import { PaymentsFiltersProvider } from "./filters";
-import { route } from "@/services/api/routes";
-import { BookingReport } from "@/types/BookingReport";
 import { Suspense } from "react";
-import { ReportProvider } from "./report-context";
 import LoadingScreen from "@/pages/loading-screen";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { getServicesSummariesData } from "../api/services/summaries/get";
+import type { SearchParams } from "next/dist/server/request/search-params";
 
-type SearchParams = { [key: string]: string | number | boolean | string[] };
-
-async function getData(searchParams: SearchParams) {
-  const params = await searchParams;
-  const path = route("INDEX_REPORTS", params ? params : {});
-  const data = await fetch(path);
-  return (await data.json()) as BookingReport[];
-}
-
-export default function Payments({
+export default async function PaymentsPage({
   searchParams,
 }: {
-  searchParams: SearchParams;
+  searchParams: Promise<SearchParams>;
 }) {
-  const dataPromise = getData(searchParams);
+  const summaries = await getServicesSummariesData(searchParams);
 
   return (
-    <ReportProvider reportPromise={dataPromise}>
-      <PaymentsFiltersProvider>
-        <div className="flex flex-col h-screen p-8 !pb-0 gap-8 sm:p-16 font-[family-name:var(--font-inter)]">
-          <header className="flex justify-between items-center gap-4">
-            <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
-              Payments
-            </h1>
-            <DateFilters />
-          </header>
-          <ScrollArea
-            className="rounded-t-xl border flex-1 overflow-auto"
-            style={{ scrollbarWidth: "none" }}
-          >
-            <Suspense fallback={<LoadingScreen />}>
-              <BookingsTable columns={columns} />
-            </Suspense>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-        </div>
-      </PaymentsFiltersProvider>
-    </ReportProvider>
+    <PaymentsFiltersProvider>
+      <div className="flex flex-col h-screen p-8 !pb-0 gap-8 sm:p-16 font-[family-name:var(--font-inter)]">
+        <header className="flex justify-between items-center gap-4">
+          <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
+            Payments
+          </h1>
+          <DateFilters />
+        </header>
+        <ScrollArea
+          className="rounded-t-xl border flex-1 overflow-auto"
+          style={{ scrollbarWidth: "none" }}
+        >
+          <Suspense fallback={<LoadingScreen />}>
+            <BookingsTable columns={columns} data={summaries} />
+          </Suspense>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+    </PaymentsFiltersProvider>
   );
 }
